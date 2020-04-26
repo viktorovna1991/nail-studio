f402b7f8bd00334a00e413c309aa596f0ef1ef6f
viktorovna1991 <viktorovna1991@gmail.com>
2020-04-26 22:11:55 +0300

    added menu

--- projects/user-portal/src/app/app.module.ts
+++ projects/user-portal/src/app/app.module.ts
@@ -1,18 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { SystemComponent } from './system/system.component';
 import { MainComponent } from './system/main/main.component';
+import { HeaderComponent } from './system/header/header.component';
+import {MatTabsModule} from '@angular/material/tabs';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {FormsModule} from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     SystemComponent,
-    MainComponent
+    MainComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    MatTabsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatToolbarModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
